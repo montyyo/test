60c6894c507c64031a9785078ed8f8f9dc189ead
marco <marcoiban@gmail.com>
2015-01-13 16:04:54 +0100

    metodo para obtener la suma de numeros en un rango

--- Test.java
+++ Test.java
@@ -20,9 +20,23 @@ public class Test
       }
    }
    
+   /**
+    * . Implementa un método llamado sumValues que saque por pantalla la suma
+    * de los números comprendidos entre el 0 y el 10 (ambos incluidos) haciendo uso de un bucle while.
+    */
    public void sumValues()
    {
-      
+      int lim_sup = 0;//limite inferior
+      int lim_inf= 10;//limite superior
+      int sum=0;//acumulador
+      while(lim_sup <= lim_inf) //condicion
+      {
+          
+          sum = lim_sup + sum; //suma de acumulador 
+          lim_sup= lim_sup+1; //suma a limite hasta llegar a limite superior
+          
+      }
+      System.out.println( "la suma de los digitos entre 0 y 10 es : " + sum);
        
        
    }
